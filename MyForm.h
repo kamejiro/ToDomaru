3dd256fa4f42a024c8c0a8f5bab27248a3f363ad
kamejiro <kameoutput0529@gmail.com>
2021-02-02 22:03:51 +0900

    2月のバックアップ。UIを作成

--- MyForm.h
+++ MyForm.h
@@ -34,8 +34,17 @@ namespace ToDomaru {
 				delete components;
 			}
 		}
-	private: System::Windows::Forms::TextBox^ textBox1;
+
 	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Button^ button3;
+	private: System::Windows::Forms::Button^ button4;
+	private: System::Windows::Forms::MonthCalendar^ monthCalendar1;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::ListBox^ listBox1;
+	private: System::Windows::Forms::Button^ button5;
+
+
 	protected:
 
 	private:
@@ -51,34 +60,105 @@ namespace ToDomaru {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
 			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->button4 = (gcnew System::Windows::Forms::Button());
+			this->monthCalendar1 = (gcnew System::Windows::Forms::MonthCalendar());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->listBox1 = (gcnew System::Windows::Forms::ListBox());
+			this->button5 = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
-			// 
-			// textBox1
-			// 
-			this->textBox1->Location = System::Drawing::Point(35, 24);
-			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(100, 19);
-			this->textBox1->TabIndex = 0;
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(38, 106);
+			this->button1->Location = System::Drawing::Point(566, 24);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->Size = System::Drawing::Size(38, 38);
 			this->button1->TabIndex = 1;
-			this->button1->Text = L"button1";
+			this->button1->Text = L"並び替え";
 			this->button1->UseVisualStyleBackColor = true;
 			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(610, 24);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(38, 38);
+			this->button2->TabIndex = 2;
+			this->button2->Text = L"編集";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
+			// 
+			// button3
+			// 
+			this->button3->Location = System::Drawing::Point(654, 24);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(38, 38);
+			this->button3->TabIndex = 3;
+			this->button3->Text = L"追加";
+			this->button3->UseVisualStyleBackColor = true;
+			this->button3->Click += gcnew System::EventHandler(this, &MyForm::button3_Click);
+			// 
+			// button4
+			// 
+			this->button4->Location = System::Drawing::Point(522, 24);
+			this->button4->Name = L"button4";
+			this->button4->Size = System::Drawing::Size(38, 38);
+			this->button4->TabIndex = 4;
+			this->button4->Text = L"削除";
+			this->button4->UseVisualStyleBackColor = true;
+			this->button4->Click += gcnew System::EventHandler(this, &MyForm::button4_Click);
+			// 
+			// monthCalendar1
+			// 
+			this->monthCalendar1->Location = System::Drawing::Point(72, 243);
+			this->monthCalendar1->Name = L"monthCalendar1";
+			this->monthCalendar1->TabIndex = 5;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(70, 89);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(35, 12);
+			this->label1->TabIndex = 6;
+			this->label1->Text = L"label1";
+			//this->label1->Click += gcnew System::EventHandler(this, &MyForm::label1_Click);
+			// 
+			// listBox1
+			// 
+			this->listBox1->FormattingEnabled = true;
+			this->listBox1->ItemHeight = 12;
+			this->listBox1->Location = System::Drawing::Point(357, 89);
+			this->listBox1->Name = L"listBox1";
+			this->listBox1->Size = System::Drawing::Size(335, 316);
+			this->listBox1->TabIndex = 7;
+			//this->listBox1->SelectedIndexChanged += gcnew System::EventHandler(this, &MyForm::listBox1_SelectedIndexChanged);
+			// 
+			// button5
+			// 
+			this->button5->Location = System::Drawing::Point(434, 24);
+			this->button5->Name = L"button5";
+			this->button5->Size = System::Drawing::Size(76, 38);
+			this->button5->TabIndex = 8;
+			this->button5->Text = L"切り替え";
+			this->button5->UseVisualStyleBackColor = true;
+			this->button5->Click += gcnew System::EventHandler(this, &MyForm::button5_Click);
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 12);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(284, 261);
+			this->ClientSize = System::Drawing::Size(704, 461);
+			this->Controls->Add(this->button5);
+			this->Controls->Add(this->listBox1);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->monthCalendar1);
+			this->Controls->Add(this->button4);
+			this->Controls->Add(this->button3);
+			this->Controls->Add(this->button2);
 			this->Controls->Add(this->button1);
-			this->Controls->Add(this->textBox1);
 			this->Name = L"MyForm";
 			this->Text = L"MyForm";
 			this->ResumeLayout(false);
@@ -87,7 +167,20 @@ namespace ToDomaru {
 		}
 #pragma endregion
 	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
-		MessageBox::Show(textBox1->Text + "さん、こんにちは。");
+		MessageBox::Show("もんきちさん、こんにちは。ソートボタンです");
 	}
-	};
+
+private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+	MessageBox::Show("もんきちさん、こんにちは。編集ボタンです");
+}
+private: System::Void button3_Click(System::Object^ sender, System::EventArgs^ e) {
+	MessageBox::Show("もんきちさん、こんにちは。追加ボタンです");
+}
+private: System::Void button4_Click(System::Object^ sender, System::EventArgs^ e) {
+	MessageBox::Show("もんきちさん、こんにちは。削除ボタンです");
+}
+private: System::Void button5_Click(System::Object^ sender, System::EventArgs^ e) {
+	MessageBox::Show("もんきちさん、こんにちは。切り替えボタンです");
+}
+};
 }
